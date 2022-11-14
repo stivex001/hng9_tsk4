6e1843a77d859be3b3c8b045c14bab7ffbbaac32
stivex001 <stephenadeyemo@gmail.com>
2022-11-14 15:09:04 +0100

    changed the images to .png because of seo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import CTO from "../components/CTO";
 import Footer from "../components/Footer/Footer";
-import Header from "../components/Header";
+import Header from "../components/Header/Header";
 import Hero from "../components/Hero";
 import Nft from "../components/Nft";
 import Places from "../components/Places";
@@ -9,14 +9,10 @@ import Places from "../components/Places";
 const Home = () => {
   return (
     <>
-      <div className="container">
-        <Header />
-        <Hero />
-      </div>
+      <Header />
+      <Hero />
       <CTO />
-      <div className="container">
-        <Places />
-      </div>
+      <Places />
       <Nft />
       <Footer />
     </>
